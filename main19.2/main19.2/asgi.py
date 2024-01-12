af36587b36000b7871f8d168eb1abfc3737f370a
ylug <145848823+ylug@users.noreply.github.com>
2024-01-12 15:52:17 +0500

    Update asgi.py

--- main19.2/main19.2/asgi.py
+++ main19.2/main19.2/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw192.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main19.2.settings')
 
 application = get_asgi_application()
