fef845a042235654c2032bdf221b7247c5436573
ylug <145848823+ylug@users.noreply.github.com>
2024-01-12 16:09:26 +0500

    Update manage.py

--- main19.2/manage.py
+++ main19.2/manage.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw192.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main19.2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
